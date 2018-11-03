23463389ff421ec6deefb53511c6d3302b626c1a
halomademeapc <opr9029@gmail.com>
2018-11-03 12:22:59 -0400

    set up transparent button outlines

--- src/app/modules/components/components.module.ts
+++ src/app/modules/components/components.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FloatySpaceRingUiModule } from '../floaty-space-ring-ui/floaty-space-ring-ui.module';
 
 @NgModule({
   declarations: [NavbarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FloatySpaceRingUiModule
   ],
   exports: [
     NavbarComponent
--- src/app/modules/floaty-space-ring-ui/fsr-button/fsr-button.component.ts
+++ src/app/modules/floaty-space-ring-ui/fsr-button/fsr-button.component.ts
@@ -7,11 +7,13 @@ import { Component, Input, HostListener, ElementRef, AfterViewInit } from '@angu
 })
 export class FsrButtonComponent {
   @Input() size: string;
+  @Input() color = 'primary';
+  @Input() solid = true;
   private gradient: { x: number, y: number } = { x: 0, y: 0 };
 
-  constructor(public el: ElementRef<HTMLElement>) { }
+  constructor(public el: ElementRef<HTMLElement>) { console.log(this.solid); }
 
-  @HostListener('mousemove', ['$event'])
+  @HostListener('document:mousemove', ['$event'])
   onmousemove = (event: MouseEvent): { x: number, y: number } =>
     this.gradient = {
       x: event.pageX - this.el.nativeElement.offsetLeft,
@@ -19,14 +21,26 @@ export class FsrButtonComponent {
     }
 
   private getBackgroundGradient = () => {
-    return {
-      'background': `radial-gradient(circle at ${this.gradient.x}px ${this.gradient.y}px, #8941f4, #5219a8)`
-    };
+    if (this.solid) {
+      return {
+        'background': `radial-gradient(circle at ${this.gradient.x}px ${this.gradient.y}px, #8941f4, #5219a8)`
+      };
+    } else {
+      return {
+        'background': `radial-gradient(circle at ${this.gradient.x}px ${this.gradient.y}px, rgba(255,255,255,.2) 0%, transparent 50%)`
+      };
+    }
   }
 
   private getBorderGradient = () => {
-    return {
-      'background': `radial-gradient(circle at ${this.gradient.x}px ${this.gradient.y}px, #ab7af4 0%, #8941f4 20%, #5219a8 100%)`
-    };
+    if (this.color === 'primary') {
+      return {
+        'background': `radial-gradient(circle at ${this.gradient.x}px ${this.gradient.y}px, #ab7af4 0%, #8941f4 20%, #5219a8 100%)`
+      };
+    } else {
+      return {
+        'background': `radial-gradient(circle at ${this.gradient.x}px ${this.gradient.y}px, rgba(255,255,255,.6) 0%, transparent 50%)`
+      };
+    }
   }
 }
