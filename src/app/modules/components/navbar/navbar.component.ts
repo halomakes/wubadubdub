60c2bea6082bb4a4cd03a33cb365866283734c6c
halomademeapc <opr9029@gmail.com>
2018-11-03 16:44:25 -0400

    scroll to top on page change

--- src/app/modules/components/navbar/navbar.component.ts
+++ src/app/modules/components/navbar/navbar.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { NavbarService } from '../navbar.service';
+import { NgAnimateScrollService } from 'ng-animate-scroll';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
-  constructor(public navbar: NavbarService) { }
+  constructor(public navbar: NavbarService, private scroll: NgAnimateScrollService) { }
 
-  ngOnInit() {
-  }
-
+  scrollToTop = (): void => this.scroll.scrollToElement('top', 500);
 }
