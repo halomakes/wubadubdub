56c3f0f17192782dd9f7df381be69ead52235300
halomademeapc <opr9029@gmail.com>
2018-11-04 12:10:50 -0500

    move specialties to json file

--- src/app/modules/pages/skills/skills.component.ts
+++ src/app/modules/pages/skills/skills.component.ts
@@ -3,6 +3,7 @@ import { NavbarService } from '../../components/navbar.service';
 import { LanguageIcon } from './language-icon';
 import { HttpClient } from '@angular/common/http';
 import { Subscription } from 'rxjs';
+import { Specialty } from './specialty';
 
 @Component({
   selector: 'app-skills',
@@ -13,19 +14,26 @@ export class SkillsComponent implements OnInit {
 
   constructor(public navbar: NavbarService, private http: HttpClient) { }
   languages: Array<LanguageIcon>;
+  specialties: Array<Specialty>;
 
   ngOnInit() {
     this.navbar.makeOpaque();
     this.loadLanguages();
+    this.loadSpecialties();
   }
 
   private loadLanguages = (): Subscription =>
     this.http.get<LanguageIcon[]>('assets/data/languages.json')
       .subscribe(r => this.languages = r)
 
+  private loadSpecialties = (): Subscription =>
+    this.http.get<Specialty[]>('assets/data/specialties.json')
+      .subscribe(r => this.specialties = r)
+
   getStyle = (l: LanguageIcon) => {
     return {
-      'background-image': `url('assets/svg/icon/${l.fileName}.svg')`
+      'background-image': `url('${this.getImageUri(l.fileName)}')`
     };
   }
-}
+
+  getImageUri = (fileName: string) => `assets/svg/icon/${fileName}.svg`;
