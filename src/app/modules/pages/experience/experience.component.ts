9c831945dd0fefe85f3bf38275858bb4b98cb28e
halomademeapc <opr9029@gmail.com>
2018-11-05 18:49:51 -0500

    display data

--- src/app/modules/pages/experience/experience.component.ts
+++ src/app/modules/pages/experience/experience.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription, Observable, forkJoin } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Experience } from './experience';
+import { Education } from './education';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-experience',
@@ -6,10 +11,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experience.component.scss']
 })
 export class ExperienceComponent implements OnInit {
+  private events: Array<Experience>;
+  private work: Array<Experience>;
+  private school: Array<Education>;
+  private baseUrl = 'assets/data/experience/';
 
-  constructor() { }
+  showWork = true;
+  showSchool = true;
+  showOther = true;
+
+  hasLoaded = false;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.loadAllData().subscribe(r => this.hasLoaded = true);
   }
 
+  private loadEvents = (): Observable<Experience[]> =>
+    this.http.get<Experience[]>(`${this.baseUrl}events.json`)
+      .pipe(map(r => this.events = r))
+
+  private loadWork = (): Observable<Experience[]> =>
+    this.http.get<Experience[]>(`${this.baseUrl}work.json`)
+      .pipe(map(r => this.work = r))
+
+  private loadSchool = (): Observable<Education[]> =>
+    this.http.get<Education[]>(`${this.baseUrl}school.json`)
+      .pipe(map(r => this.school = r))
+
+  private loadAllData = () =>
+    forkJoin([this.loadEvents(), this.loadWork(), this.loadSchool()]);
+
+  getOrderedEvents = (): Array<Experience | Education> =>
+    this.getEventsToDisplay().sort(this.sortByDateDescending)
+
+  private getEventsToDisplay = (): Array<Experience | Education> => {
+    let base: Array<Experience | Education> = [];
+    base = this.showWork ? base.concat(this.work) : base;
+    base = this.showSchool ? base.concat(this.school) : base;
+    base = this.showOther ? base.concat(this.events) : base;
+    return base;
+  }
+
+  getMinimumDate = (): Date => this.getOrderedEvents()
+    .sort(this.sortByDateAscending)[0].date
+
+  sortByDateDescending = (a, b) => new Date(b.date).getDate() - new Date(a.date).getDate();
+  sortByDateAscending = (a, b) => -this.sortByDateDescending(a, b);
+
+  logThing = (): void => {
+    const data = this.getOrderedEvents();
+    console.log(data);
+    console.log(this.getMinimumDate());
+  }
 }
