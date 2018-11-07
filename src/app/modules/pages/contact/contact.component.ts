a66d77b93f579dc84d219a0eb5305d2ff48349bb
halomademeapc <opr9029@gmail.com>
2018-11-07 00:19:48 -0500

    add links to contact page

--- src/app/modules/pages/contact/contact.component.ts
+++ src/app/modules/pages/contact/contact.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Tile } from '../tile';
+import { HttpClient } from '@angular/common/http';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  tiles: Array<Tile>;
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
 
   ngOnInit() {
+    this.loadLinks();
   }
 
+  loadLinks = (): Subscription =>
+    this.http.get<Tile[]>('assets/data/contact/social.json')
+      .subscribe(d => this.tiles = d)
 }
